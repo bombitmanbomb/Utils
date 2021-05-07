5bccfde10da2a45f637f1f33bba8d3e4636471a3
Ethan Snow <bombitmanbomb@gmail.com>
2021-05-07 02:57:16 -0400

    Create TimeSpan.ts

---
+++ src/TimeSpan.ts
@@ -0,0 +1,23 @@
+export class TimeSpan {
+  msecs: number
+  constructor(num:number) {
+    this.msecs = num != null ? num : 0
+  }
+  public static fromSeconds(num:number):number {
+    return num * 1000
+  }
+
+  public static fromMinutes(num:number):number {
+    return num * 60000
+  }
+
+  public static Delay(timespan:TimeSpan | number) {
+    let Time:TimeSpan
+    if (!(timespan instanceof TimeSpan)) {
+      Time = new TimeSpan(timespan)
+    } else {
+      Time = timespan
+    }
+    return new Promise((resolve) => setTimeout(resolve, Time.msecs))
+  }
+}
