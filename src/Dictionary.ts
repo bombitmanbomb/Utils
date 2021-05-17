36fd71fbcbde493753437b221e8d7e521b5edae2
Ethan Snow <bombitmanbomb@gmail.com>
2021-05-17 02:18:24 -0400

    Update Dictionary.ts

--- src/Dictionary.ts
+++ src/Dictionary.ts
@@ -15,6 +15,7 @@ export class Dictionary<T extends string | number, A> extends Array {
 	 * Add a key to the Dictionary.
 	 */
 	public Add(Key: T, Value: A): boolean {
+		if (Key == null) return false
 		if (this.ContainsKey(Key))
 			throw new Error(
 				"ArgumentException: An element with the same key already exists"
