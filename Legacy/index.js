a9c4cf7c5ceddaebc8b672842f3e4e5af09e8e4f
bombitmanbomb <bombitmanbomb@gmail.com>
2021-07-06 03:19:46 +0000

    Deploying to gh-pages from @ bombitmanbomb/Utils@253e49757905603f8ae63a84c93d6483ee4e600f 🚀

---
+++ Legacy/index.js
@@ -0,0 +1,6 @@
+const { Dictionary } = require("./Dictionary");
+const { Enumerable } = require("./Enumerable");
+const { List } = require("./List");
+const { Out } = require("./Out");
+const { TimeSpan } = require("./TimeSpan");
+module.exports = { Dictionary, Enumerable, List, Out, TimeSpan };
