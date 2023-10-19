03932e730e86e5ca6d48967dc3967aafd46f7e1a
Ethan Snow <bombitmanbomb@gmail.com>
2023-10-19 10:22:50 -0400

    omit self from stack trace

--- src/CustomError.ts
+++ src/CustomError.ts
@@ -8,6 +8,12 @@ export class CustomError extends Error {
 		(this as any)[kCode as unknown as string] = type;
 		if ((Error as any).captureStackTrace)
 			(Error as any).captureStackTrace(this, CustomError);
+		this.stack = this.stack
+			?.split("\n")
+			.filter(function (line, index) {
+				return index !== 1;
+			})
+			.join("\n");
 	}
 
 	get name() {
