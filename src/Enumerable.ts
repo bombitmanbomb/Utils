f24c9d21941816ff0c9d695a9211b290e90f2cc7
bombitmanbomb <bombitmanbomb@gmail.com>
2024-04-19 07:36:38 +0000

    Deploying to gh-pages from @ bombitmanbomb/Utils@c254046226a9f51ed073075d26a5abb02d359330 🚀

--- src/Enumerable.ts
+++ src/Enumerable.ts
@@ -4,37 +4,37 @@ import { List } from "./List";
  * Enumerable Util Class (for Non TS)
  */
 export class Enumerable<T extends string | number> extends Object {
-	constructor(list: T[] | List<T>) {
-		super();
-		if (list instanceof List || list instanceof Array || Array.isArray(list)) {
-			for (let i = 0; i < list.length; i++) {
-				Object.defineProperty(this, list[i], { value: i });
-			}
-		} else if (list != null) {
-			const keys = Object.keys(list);
-			for (let i = 0; i < keys.length; i++) {
-				Object.defineProperty(this, keys[i], { value: list[keys[i]] });
-			}
-		} else {
-			throw new CustomError(
-				"InvalidConstructor",
-				"Expected type: <Array, List, Object>",
-			);
-		}
-		Object.freeze(this);
-	}
-	/**
+  constructor(list: T[] | List<T>) {
+    super();
+    if (list instanceof List || list instanceof Array || Array.isArray(list)) {
+      for (let i = 0; i < list.length; i++) {
+        Object.defineProperty(this, list[i], { value: i });
+      }
+    } else if (list != null) {
+      const keys = Object.keys(list);
+      for (let i = 0; i < keys.length; i++) {
+        Object.defineProperty(this, keys[i], { value: list[keys[i]] });
+      }
+    } else {
+      throw new CustomError(
+        "InvalidConstructor",
+        "Expected type: <Array, List, Object>",
+      );
+    }
+    Object.freeze(this);
+  }
+  /**
 	 * Get Value Raw
 	 */
-	public GetValue(Key: T): string | number {
-		const { [Key]: Identifier } = this;
-		return Identifier as unknown as string | number;
-	}
+  public GetValue(Key: T): string | number {
+    const { [Key]: Identifier } = this;
+    return Identifier as unknown as string | number;
+  }
 
-	public FromNumber(index: number): string | undefined {
-		const keys = Object.keys(this);
-		if (keys.length - 1 < index) throw Error("Index larger than Enum");
-		const { [keys[index] as T]: Identifier } = this;
-		if (Identifier != null) return keys[index];
-	}
+  public FromNumber(index: number): string | undefined {
+    const keys = Object.keys(this);
+    if (keys.length - 1 < index) throw Error("Index larger than Enum");
+    const { [keys[index] as T]: Identifier } = this;
+    if (Identifier != null) return keys[index];
+  }
 }
--- src/Uri.ts
+++ src/Uri.ts
@@ -2,64 +2,64 @@
  * Uri Support Class for working with Url's much easier by converting it to a usable object
  */
 export class Uri {
-	public Segments: string[];
-	public _raw: Component;
-	public rawUrl: string;
-	constructor(url: string) {
-		this.rawUrl = "";
-		this._raw = { path: "" };
-		this.Segments = [];
-		this.URL = url;
-	}
-	public set URL(value: string) {
-		Object.defineProperties(this, {
-			rawUrl: { value, enumerable: false },
-			_raw: { value: uri_parse(value), enumerable: false },
-		});
-		this.Segments = [];
-		const path = this._raw.path.split("/");
-		path.forEach((value, index) => {
-			this.Segments.push(index < path.length - 1 ? value + "/" : value);
-		});
-	}
-	public get URL(): string {
-		return this.rawUrl;
-	}
-	public get Host(): string {
-		return this._raw.host as string;
-	}
-	public get Scheme(): string {
-		return this._raw.scheme as string;
-	}
-	static EscapeDataString(dat: string): string {
-		return encodeURI(dat);
-	}
-	public get Query(): string {
-		return this._raw.query
-			? "?" + this._raw.query
-			: (null as unknown as string);
-	}
+  public Segments: string[];
+  public _raw: Component;
+  public rawUrl: string;
+  constructor(url: string) {
+    this.rawUrl = "";
+    this._raw = { path: "" };
+    this.Segments = [];
+    this.URL = url;
+  }
+  public set URL(value: string) {
+    Object.defineProperties(this, {
+      rawUrl: { value, enumerable: false },
+      _raw: { value: uri_parse(value), enumerable: false },
+    });
+    this.Segments = [];
+    const path = this._raw.path.split("/");
+    path.forEach((value, index) => {
+      this.Segments.push(index < path.length - 1 ? value + "/" : value);
+    });
+  }
+  public get URL(): string {
+    return this.rawUrl;
+  }
+  public get Host(): string {
+    return this._raw.host as string;
+  }
+  public get Scheme(): string {
+    return this._raw.scheme as string;
+  }
+  static EscapeDataString(dat: string): string {
+    return encodeURI(dat);
+  }
+  public get Query(): string {
+    return this._raw.query
+      ? "?" + this._raw.query
+      : (null as unknown as string);
+  }
 }
 function uri_parse(uriString: string): Component {
-	const components: Component = { path: "" };
-	const matches = uriString.match(
-		/^(?:([^:/?#]+):)?(?:\/\/((?:([^/?#@]*)@)?(\[[^/?#\]]+\]|[^/?#:]*)(?::(\d*))?))?([^?#]*)(?:\?([^#]*))?(?:#((?:.|\n|\r)*))?/i,
-	);
-	if (matches) {
-		components.scheme = matches[1];
-		components.userinfo = matches[3];
-		components.host = matches[4];
-		components.port = parseInt(matches[5], 10);
-		components.path = matches[6] ?? "";
-		components.query = matches[7];
-		components.fragment = matches[8];
-		if (isNaN(components.port)) {
-			components.port = matches[5] as unknown as number;
-		}
-	} else {
-		components.error = components.error ?? "URI can not be parsed.";
-	}
-	return components;
+  const components: Component = { path: "" };
+  const matches = uriString.match(
+    /^(?:([^:/?#]+):)?(?:\/\/((?:([^/?#@]*)@)?(\[[^/?#\]]+\]|[^/?#:]*)(?::(\d*))?))?([^?#]*)(?:\?([^#]*))?(?:#((?:.|\n|\r)*))?/i,
+  );
+  if (matches) {
+    components.scheme = matches[1];
+    components.userinfo = matches[3];
+    components.host = matches[4];
+    components.port = parseInt(matches[5], 10);
+    components.path = matches[6] ?? "";
+    components.query = matches[7];
+    components.fragment = matches[8];
+    if (isNaN(components.port)) {
+      components.port = matches[5] as unknown as number;
+    }
+  } else {
+    components.error = components.error ?? "URI can not be parsed.";
+  }
+  return components;
 }
 
 interface Component {
